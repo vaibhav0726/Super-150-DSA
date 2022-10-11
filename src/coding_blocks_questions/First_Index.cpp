8dee2b67b757ceff45482827715b929729c99bec
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-10-11 23:49:40 +0530

    Create First_Index.cpp

---
+++ src/coding_blocks_questions/First_Index.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int *arr, int i, int M, int n){
+	if(i >= n) return -1;
+	if(arr[i] == M){
+		return i;
+	}
+	return f(arr, i+1, M, n);
+}
+
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	int M;
+	cin>>M;
+	cout<<f(arr, 0, M, n);
+	return 0;
+}
